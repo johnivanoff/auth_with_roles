58a51d9449aea5d2e3f3201bc68ae12ec3223edc
unknown <joiva@.BRIGGSEQUIPMENT.COM>
2010-05-27 13:37:28 -0500

    auth_with_roles Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '27528d768e7d504a82329e8b701d6144393df3a4701f9054e8be970fa89bdbf88e99b7308ec6862d4f0b9fcf2b06a1d6b8348510919cc39d7dcd8537680dbb40'
