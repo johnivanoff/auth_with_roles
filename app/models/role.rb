4d06edffa27d4f1105f498fbf8b1e9dbd08fa00a
unknown <joiva@.BRIGGSEQUIPMENT.COM>
2010-06-03 10:31:32 -0500

    added validation rules

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,7 @@
 class Role < ActiveRecord::Base
   has_many :assignments
   has_many :users, :through => :assignments
+  
+  validates :name, :presence => true
+  validates :name, :uniqueness => true
 end
